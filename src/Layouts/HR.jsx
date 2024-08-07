e8a7bcbe79145139c9559bdd43f080ea8c9ef92c
Dave <dave051621@gmail.com>
2024-08-07 14:28:54 +0300

    Application Page is inserted

--- src/Layouts/HR.jsx
+++ src/Layouts/HR.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import NewPost from "../Components/NewPost";
 import Preview from "../Components/Preview";
+import ApplicationS from "../Components/Application";
 
 function HR() {
     return (
       <>
       <Navbar/>
-        <Router>
+      <BrowserRouter>
+        <ApplicationS/>
+      </BrowserRouter>
+        {/*<Router>
             <Routes>
                 <Route path="/" element={<NewPost />} />
                 <Route path="/preview" element= {<Preview/>}/>
             </Routes>
-        </Router>
+        </Router>*/}
             <Footer/>
       </>
     );
